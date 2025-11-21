e0b0b60f0cf2e6cdfa40340ce56f038abfb53acd
priyankakumbhar01 <priyankakumbhar0101000@gmail.com>
2025-11-21 14:44:52 +0530

    Update urls.py

--- bloodbank/urls.py
+++ bloodbank/urls.py
@@ -1,19 +1,22 @@
-from django.urls import path
-from . import views
-urlpatterns = [path('', views.index, name='bloodbank_index')]
-
 # bloodbank/urls.py
+
 from django.urls import path, include
+
 from rest_framework.routers import DefaultRouter
-from .views import DonorViewSet, BloodInventoryViewSet, IssueBloodAPIView, UsageReportAPIView, SummaryReportAPIView
 
+from .views import BloodDonorViewSet, BloodBagViewSet, BloodRequestViewSet
+ 
 router = DefaultRouter()
-router.register(r'donors', DonorViewSet, basename='donor')
-router.register(r'inventory', BloodInventoryViewSet, basename='inventory')
 
+router.register(r'donors', BloodDonorViewSet, basename='blood-donor')
+
+router.register(r'bags', BloodBagViewSet, basename='blood-bag')
+
+router.register(r'requests', BloodRequestViewSet, basename='blood-request')
+ 
 urlpatterns = [
+
     path('', include(router.urls)),
-    path('issue/', IssueBloodAPIView.as_view(), name='blood-issue'),
-    path('usage/', UsageReportAPIView.as_view(), name='blood-usage'),
-    path('reports/summary/', SummaryReportAPIView.as_view(), name='blood-summary'),
+
 ]
+ 
